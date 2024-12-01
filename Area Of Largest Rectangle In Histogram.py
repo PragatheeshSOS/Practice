13efe485d1cc2800455f0a5fa89b193cc9d9b837
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2024-12-01 06:46:01 +0530

    Commit
    Commit

---
+++ Area Of Largest Rectangle In Histogram.py
@@ -0,0 +1,42 @@
+#AREA OF LARGEST RECTANGLE IN HISTOGRAM......(All Pass Only On Portal)
+
+def forward(current):
+    now = given[current]
+    location = current
+    for i in range(current,len(given)):
+        if now<=given[i]:
+            location = i
+        else:
+            break
+    return location
+    
+def backward(current):
+    now = given[current]
+    location = current
+    for i in range(current,-1,-1):
+        if now<=given[i]:
+            location = i
+        else:
+            break
+    return location
+
+given,final = eval(input()),[]
+for i in range(len(given)):
+    final.append(((forward(i)-backward(i))+1)*given[i])
+print(max(final))
+
+'''
+
+
+INPUT:
+[2,1,5,6,2,3]
+OUTPUT:
+10
+
+INPUT:
+[2,4]
+OUTPUT:
+4
+
+
+'''

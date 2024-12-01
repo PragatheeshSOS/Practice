cb8ddbf0efe4afc04d02e3245d4e3dfb3ac62a07
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2024-12-01 06:53:55 +0530

    Commit
    Write A Python Program To Find The Area Of Largest Rectangle In Histogram.?

--- Area Of Largest Rectangle In Histogram.py
+++ Area Of Largest Rectangle In Histogram.py
@@ -1,5 +1,4 @@
 #AREA OF LARGEST RECTANGLE IN HISTOGRAM......(All Pass Only On Portal)
-
 def forward(current):
     now = given[current]
     location = current
@@ -9,7 +8,7 @@ def forward(current):
         else:
             break
     return location
-    
+
 def backward(current):
     now = given[current]
     location = current
@@ -24,17 +23,13 @@ given,final = eval(input()),[]
 for i in range(len(given)):
     final.append(((forward(i)-backward(i))+1)*given[i])
 print(max(final))
-
 '''
 INPUT:
 [2,1,5,6,2,3]
 OUTPUT:
 10
-
 INPUT:
 [2,4]
 OUTPUT:
 4
-
-
 '''

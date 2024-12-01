2158ec9296224c86c4423f13df75e10732ac9a35
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2024-12-01 12:41:38 +0530

    Commit

---
+++ Left Triangle Pattern Chr.py
@@ -0,0 +1,15 @@
+#Left Triangle Pattern Chr.....
+[print((chr(i+65)+(" "))*(i+1)) for i in range(int(input()))]
+'''
+INPUT - 3
+OUTPUT
+A
+B B
+C C C
+INPUT - 4
+OUTPUT
+A
+B B
+C C C
+D D D D
+'''

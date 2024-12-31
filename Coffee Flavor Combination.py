4923fde89168ac186e3a3ce75353e7c88a9a04c7
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2024-12-31 22:22:55 +0530

    Commit

--- Coffee Flavor Combination.py
+++ Coffee Flavor Combination.py
@@ -4,6 +4,17 @@ flav,comb,res = int(input()),int(input()),0
 for i in range(1,comb+1):
     res+= (math.factorial(flav)//(math.factorial(i)*math.factorial(flav-i)))
 print(res)
+
+#If Combination Library Works.....
+import math
+n = int(input())
+print(math.comb(n,int(input())) + n)
+# If Above One Fails Testcases.
+import math
+n,k,res = int(input()),int(input()),0
+for i in range(1,k+1):
+    res+=math.comb(n,i)
+print(res)
 '''
 INPUT:
 5
@@ -15,4 +26,10 @@ INPUT:
 1
 OUTPUT:
 10
+'''
+'''
+Combination Mathematical Formula:
+comb(n,k) = n! // k!(n-k)!
+For Program, We Replace k with iterative i (From 1 to k+1) (i.e).
+comb(n,k) = n! // i!(n-i)! Where i iterative loop from 1 to k+1
 '''

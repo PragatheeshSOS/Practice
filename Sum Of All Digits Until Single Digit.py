3cb11c8daa073b8e8ac2cf8b7a4191874c64c132
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-01-27 22:19:58 +0530

    Commit

--- Sum Of All Digits Until Single Digit.py
+++ Sum Of All Digits Until Single Digit.py
@@ -1,4 +1,5 @@
 #Sum Of All Digits Until Single Digit......
+#Approach - 1..................
 number = int(input())
 while(number>0):
     if number<10:
@@ -11,6 +12,13 @@ while(number>0):
             fin+=(res%10)
             res//=10
         number = fin
+#Approach - 2..................
+number = int(input())
+while(number>0):
+    if number<10:
+        print(number)
+        exit(0)
+    number = sum([int(i) for i in str(number)])
 '''
 INPUT:
 1234

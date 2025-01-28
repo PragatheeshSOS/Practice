f3dc0b123483e845c4d44d73eb1ea5568fd17c5e
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-01-28 20:53:13 +0530

    Commit

--- X Pattern.py
+++ X Pattern.py
@@ -1,4 +1,5 @@
 #X Pattern..............
+#Approach 1....................
 size,lis = int(input()),[]
 for i in range(size):
     string = ""
@@ -12,6 +13,20 @@ for i in range(size):
     lis.append(res)
 for i in lis[-2::-1]:
     print(i)
+#Approach 2..............
+size,res = int(input()),[]
+for i in range(size):
+    lis1 = []
+    for j in range(size*2):
+        if i == j or j == (size*2)-i-2:
+            print("*",end="")
+            lis1.append("*")
+        else:
+            print(" ",end="")
+            lis1.append(" ")
+    res.append(lis1)
+    print()
+[print(*i,sep="") for i in res[-2::-1]]
 '''
 INPUT:
 3

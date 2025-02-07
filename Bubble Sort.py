c732e83f354d203df0436fd93ab8279ea06a7abe
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-02-07 05:31:13 +0530

    Commit

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -10,9 +10,13 @@ Total Number Of Comparsion = n(n-1)/2
 def bubblesort(lis):
     print()
     for i in range(len(lis)):
+        swap = False
         for j in range(len(lis)-i-1):
             if lis[j]>lis[j+1]:
                 lis[j],lis[j+1] = lis[j+1],lis[j]
+                swap = True
+        if not swap:
+            return lis
         print(*lis,end=" -> ")
         print(lis[j],lis[j+1])
     return lis
@@ -24,10 +28,11 @@ Enter Input: 2 3 4 3 4 2 4 5
 2 3 3 2 4 4 4 5 -> 4 4
 2 3 2 3 4 4 4 5 -> 4 4
 2 2 3 3 4 4 4 5 -> 3 4
-2 2 3 3 4 4 4 5 -> 3 3
-2 2 3 3 4 4 4 5 -> 2 3
-2 2 3 3 4 4 4 5 -> 2 2
-2 2 3 3 4 4 4 5 -> 2 2
 
 Final Result:  2 2 3 3 4 4 4 5
+# ---------------------------------
+Enter Input: 10 20 30 40 50
+
+### SWAP BREAK THE LOOP(O(n**2) -> O(n)) If Sorted. Making Its As Best Case. ###
+Final Result:  10 20 30 40 50
 '''

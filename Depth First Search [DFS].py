26e8361245446686a6e7b979f32e08f3d10c3c41
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-04-07 09:19:16 +0530

    Commit

---
+++ Depth First Search [DFS].py
@@ -0,0 +1,30 @@
+#Depth First Search........
+nodes,edges = map(int,input("Enter Number Of Node And Number Of Edges Separated By Space: ").split())
+dic,visited = {},[]
+for i in range(1,nodes+1):
+    dic[i] = []
+for _ in range(edges):
+    key,value = map(int,input("Enter Node And Its Neighbour Separated By Space: ").split())
+    dic[key].append(value)
+    dic[value].append(key)
+queue = [int(input("Enter Starting Node: "))]
+print("\n-------- DFS OUTPUT --------")
+while queue:
+    pop = queue.pop()
+    if pop not in visited:
+        print(pop,end=" ")
+        visited.append(pop)
+        queue.extend(reversed(dic[pop]))
+        queue.extend(reversed(dic[pop]))
+'''
+Enter Number Of Node And Number Of Edges Separated By Space: 6 5
+Enter Node And Its Neighbour Separated By Space: 1 2
+Enter Node And Its Neighbour Separated By Space: 1 3
+Enter Node And Its Neighbour Separated By Space: 2 4
+Enter Node And Its Neighbour Separated By Space: 2 5
+Enter Node And Its Neighbour Separated By Space: 3 6
+Enter Starting Node: 1
+
+-------- DFS OUTPUT --------
+1 2 4 5 3 6 
+'''

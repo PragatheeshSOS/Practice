53d159cfc78183d5831a6203831bff4a42a464c7
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-06-22 19:59:50 +0530

    Commit

--- Leap Year Or Not.py
+++ Leap Year Or Not.py
@@ -1,6 +1,6 @@
 # Leap Year Or Not.......................................................................................
 def isleap(year):
-    return (not year%4 and year%100)>0 or (not year%100 and not year%400)>0
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 print("Leap Year") if isleap(int(input("Enter Year: "))) else print("Not A Leap Year")
 '''
 INPUT:
